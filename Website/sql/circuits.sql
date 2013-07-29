132b3aec6a13d81c16d53462612e66dad617d07c
Niki Eskola <neskola@gmail.com>
2013-07-29 19:39:59 +0300

    modified a bit

--- Website/sql/circuits.sql
+++ Website/sql/circuits.sql
@@ -4,10 +4,8 @@ CREATE TABLE CIRCUITS
 	gp_name varchar(64),
 	gp_year int NOT NULL,
 	gp_number int NOT NULL,
-	gp_date date NOT NULL,
-	gp_time time NOT NULL,
-	gp_qual date NOT NULL,
-	gp_qual_time time NOT NUll,
+	gp_date TIMESTAMP NOT NULL,
+	gp_qual_date TIMESTAMP NOT NULL,	
 	gp_info varchar(512),
 	UNIQUE (gp_id),
 	CONSTRAINT gp_id PRIMARY KEY (gp_id)
--- Website/sql/drivers.sql
+++ Website/sql/drivers.sql
@@ -2,6 +2,8 @@ CREATE TABLE DRIVERS
 (
 	d_id int NOT NULL,
 	d_name varchar(64),
+	d_team varchar(64),
+	d_imgsrc varchar(255),
 	UNIQUE(d_id)
 	CONSTRAINT d_id PRIMARY KEY (d_id)
 )
