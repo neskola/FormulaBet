467a06b5fc0d8a8d26b4d2c0f510b80978791412
Niki Eskola <neskola@gmail.com>
2014-02-22 16:05:58 +0200

    trying to get firebase to work

--- Website/js/data.js
+++ Website/js/data.js
@@ -1,26 +1,16 @@
-function CalendarDataCtrl($scope, $http) {
+var f1App = angular.module("F1App", ["firebase"]);
 
-    $scope.entries = [];
+f1App.controller('CalendarDataCtrl', function ($scope, angularFire) {
+    alert('foo');
+    var ref;
+    ref = new Firebase("https://f1kaapo.firebaseio.com/calendar");
+    $scope.calendars = [];
 
-    $http.get('data/calendar.json')
-      .then(function (result) {
-          $scope.entries = result.data;
-    });
+    $scope.addCalendar = function() {
+        $scope.calendar.push($scope.newCalendar);
+        return $scope.newProp = "";
+    };
 
-}
-
-function DriverDataCtrl($scope, $http) {
-
-    $scope.entries = [];
-
-    $http.get('data/drivers.json')
-      .then(function (result) {
-          $scope.entries = result.data;
-      });
-}
-
-function LatestResults($scope, $http) {
-    $scope.entries = [ 
-        { user : "ile", qpoints: 5, gpoints : 15 }, 
-        { user : "niki", qpoints: 3, gpoints: 11 } ];
-}
+    return angularFire(ref, $scope, "calendars");
+});
+    
