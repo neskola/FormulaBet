d4753263de54313c0d90f54f9d3b1bbe00961321
Niki Eskola <neskola@gmail.com>
2014-02-22 17:01:41 +0200

    sorting kinda works. weird empty ng-repeat divs occurs.

--- Website/js/data.js
+++ Website/js/data.js
@@ -1,22 +1,37 @@
 
-angular.module('calendar', ['firebase']).controller('Calendar', ['$scope', '$firebase',
+angular.module('calendar', ['firebase'])
+      .filter("toArray", function () {
+          return function (obj) {
+              var result = [];
+              angular.forEach(obj, function (val, key) {                  
+                  result.push(val);                  
+              });              
+              return result;
+          };
+      })
+    .controller('Calendar', ['$scope', '$firebase',
   function ($scope, $firebase) {
+      // year should be fetched from this year
       var ref = new Firebase('https://f1kaapo.firebaseio.com/calendar/2014');
       $scope.calendars = $firebase(ref.limit(20));
       $scope.predicate = 'gp_number';
 
-/*      $scope.addCalendar = function () {
-          $scope.calendars.$add({
-              from: $scope.calendar, content: $scope.calendar.gp_name
-          });
-          $scope.calendars = "";
-      }
-*/
   }]);
     
-angular.module('drivers', ['firebase']).controller('Drivers', ['$scope', '$firebase',
+angular.module('drivers', ['firebase'])
+      .filter("toArray", function () {
+          return function (obj) {
+              var result = [];
+              angular.forEach(obj, function (val, key) {
+                  result.push(val);
+              });
+              return result;
+          };
+      })
+    .controller('Drivers', ['$scope', '$firebase',
   function ($scope, $firebase) {
+      // year should be fetched from this year
       var ref = new Firebase('https://f1kaapo.firebaseio.com/drivers/2014');
       $scope.drivers = $firebase(ref);
-      $scope.predicate = 'd_team';
+      $scope.predicate = '-d_id';
   }]);
