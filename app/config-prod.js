7b3bd8f04940ec658d9565490adc5579a3fcfd65
Niki Eskola <neskola@gmail.com>
2015-11-20 20:19:57 +0200

    changed configuration scripts

---
+++ app/config-prod.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var season = "2015";
+
+var debug = false;
+
+var FIREBASE = new Firebase('https://f1kaapo.firebaseio.com/' + season);
---
+++ app/config-test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var season = "2015";
+
+var debug = true;
+
+var FIREBASE = new Firebase('https://test-f1kaapo.firebaseio.com/' + season);
