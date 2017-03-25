4d4cac9bc8f3fe14d398fa59b169b9edaf901a7b
Niki Eskola <niki.eskola@veikkaus.fi>
2017-03-25 11:36:35 +0200

    fixed result and scores calculation after data structure changes

--- python/profiles.py
+++ python/profiles.py
@@ -1,5 +1,5 @@
 #!C:\Python33\python.exe -u
-#!/usr/bin/env python
+# !/usr/bin/env python
 
 import sys, getopt, json, sha
 import curl
@@ -21,105 +21,105 @@ bank = ''
 isadmin = 0
 fb = ''
 
+
 def main(argv):
-	global user, email, name, password, active, remove, bank, fb, year, firebase_url
-	
-	inputfile = ""
-	try:
-		opts, args = getopt.getopt(argv,"harxiu:e:n:p:b:f:y:", ["user=", "email=", "name=", "password=", "bank=", "fb=", "year="])
-	except getopt.GetoptError:
-		print ("profiles.py -[a|r] [x|i] -u|--user <user name> -p|--password <password> -e|--email <email> -n|--name <name>")
-		sys.exit(2)
-	for opt, arg in opts:
-		if opt == '-h':
-			print ("profiles.py -[a|r] [x] -u|--user <user name> -p|--password <password> -e|--email <email>")
-			sys.exit()
-		if opt == '-a':
-			print ("Adding / updating user.")			
-			global adduser
-			adduser = 1
-		if opt == '-x':
-			print ("Setting as inactive")
-			global active 
-			active = 0
-		if opt == '-r':
-			print ("Removing user.")
-			global remove
-			remove = 1
-		if opt == '-i':
-			print ("Is admin user.")
-			global isadmin
-			isadmin = 1
-		if opt in ("-u", "--user"):			
-			user = arg
-		if opt in ("-p", "--password"):
-			password = arg
-		if opt in ("-e", "--email"):
-			email = arg			
-		if opt in ("-n", "--name"):
-			name = arg
-		if opt in ("-b", "--bank"):
-			bank = arg
-		if opt in ("-f", "--fb"):
-			fb = arg
-		if opt in ("-y", "--year"):
-                        year = arg
-			
-	if not fb:
-		print ("No target firebase defined!!!!")
-		sys.exit()     
-	else:
-		firebase_url = "https://" + fb + ".firebaseio.com/" + year
-		print "Target firebase is " + firebase_url
+    global user, email, name, password, active, remove, bank, fb, year, firebase_url
 
-	if adduser == 1 and user == '' and password == '':
-		print ("profiles.py -a -u|--user <user name> -p|--password <password> -e|--email <email>")
-		sys.exit()
-	elif adduser == 1:
-		updateUser(user, name, email, password, bank, active, isadmin)
+    inputfile = ""
+    try:
+        opts, args = getopt.getopt(argv, "harxiu:e:n:p:b:f:y:",
+                                   ["user=", "email=", "name=", "password=", "bank=", "fb=", "year="])
+    except getopt.GetoptError:
+        print (
+        "profiles.py -[a|r] [x|i] -u|--user <user name> -p|--password <password> -e|--email <email> -n|--name <name>")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ("profiles.py -[a|r] [x] -u|--user <user name> -p|--password <password> -e|--email <email>")
+            sys.exit()
+        if opt == '-a':
+            print ("Adding / updating user.")
+            global adduser
+            adduser = 1
+        if opt == '-x':
+            print ("Setting as inactive")
+            global active
+            active = 0
+        if opt == '-r':
+            print ("Removing user.")
+            global remove
+            remove = 1
+        if opt == '-i':
+            print ("Is admin user.")
+            global isadmin
+            isadmin = 1
+        if opt in ("-u", "--user"):
+            user = arg
+        if opt in ("-p", "--password"):
+            password = arg
+        if opt in ("-e", "--email"):
+            email = arg
+        if opt in ("-n", "--name"):
+            name = arg
+        if opt in ("-b", "--bank"):
+            bank = arg
+        if opt in ("-f", "--fb"):
+            fb = arg
+        if opt in ("-y", "--year"):
+            year = arg
 
-	if remove == 1 and user == '':
-		print ("profiles.py -r -u|--user <user name>")
-		sys.exit()
-	elif remove == 1 and user != '':
-		removeUser(user)
-	
+    if not fb:
+        print ("No target firebase defined!!!!")
+        sys.exit()
+    else:
+        firebase_url = "https://" + fb + ".firebaseio.com/" + year
+        print "Target firebase is " + firebase_url
+
+    if adduser == 1 and user == '' and password == '':
+        print ("profiles.py -a -u|--user <user name> -p|--password <password> -e|--email <email>")
+        sys.exit()
+    elif adduser == 1:
+        updateUser(user, name, email, password, bank, active, isadmin)
+
+    if remove == 1 and user == '':
+        print ("profiles.py -r -u|--user <user name>")
+        sys.exit()
+    elif remove == 1 and user != '':
+        removeUser(user)
+
+
 def updateUser(user, name, email, password, bank, active, isadmin):
-	sha_pass = sha.new(password)
-	userdata = dict()
-	userdata['email'] = email
-	userdata['name'] = name
-	userdata['bank'] = bank
-	userdata['password'] = sha_pass.hexdigest()
-	userdata['active'] = active
-	userdata['isadmin'] = isadmin
-	userdata['userid'] = user
-	print "Adding user = " + user + " to list"
-	firebase.curlPut(firebase_url + "/users/" + user + ".json", json.dumps(userdata))
+    sha_pass = sha.new(password)
+    userdata = dict()
+    userdata['email'] = email
+    userdata['name'] = name
+    userdata['bank'] = bank
+    userdata['password'] = sha_pass.hexdigest()
+    userdata['active'] = active
+    userdata['isadmin'] = isadmin
+    userdata['userid'] = user
+    print "Adding user = " + user + " to list"
+    firebase.curlPut(firebase_url + "/users/" + user + ".json", json.dumps(userdata))
+
 
 def removeUser(user):
-	print "Removing " + user + " from list."
-	firebase.curlDelete(firebase_url + "/users/" + user + ".json")
+    print "Removing " + user + " from list."
+    firebase.curlDelete(firebase_url + "/users/" + user + ".json")
 
-def getUserData(firebase_url, user_id):	
-	query = "/users.json"
-	if user_id:
-		query = "/users/" + user_id + ".json"
-	print("Connecting to: " + query);
-	datalist = json.loads(firebase.curlQuery(firebase_url + query))
 
-	userlist = []	
-	if user_id:
-		#print (json.dumps(datalist))
-		userlist.append(datalist)
-	else:
-		for key in datalist:
-			#print (json.dumps(datalist[key]))
-			userlist.append(datalist[key])
-	
-	return userlist
+def getUserData(firebase_url, user_id):
+    query = "/users/" + user_id + ".json"
+    print("Connecting to: " + query)
+    datalist = json.loads(firebase.curlQuery(firebase_url + query))
+    userlist = []
 
+    if user_id:
+        userlist.append(datalist)
+    else:
+        for key in datalist:
+            userlist.append(datalist[key])
+    return userlist
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
-	print "\n"
+    main(sys.argv[1:])
+    print "\n"
--- python/scores.py
+++ python/scores.py
@@ -10,241 +10,258 @@ from datetime import datetime, timedelta
 __CHECK_BETS = 1
 __PUSH_RESULTS = 2
 
-__GP_OPEN   = 1 # is open
-__GP_CLOSED = 2 # closed and bets are checked
-__GP_QUAL   = 3 # closed and qualification results are calculated
-__GP_RACE   = 4 # closed and race results are calculated
+__GP_OPEN = 1  # is open
+__GP_CLOSED = 2  # closed and bets are checked
+__GP_QUAL = 3  # closed and qualification results are calculated
+__GP_RACE = 4  # closed and race results are calculated
 
-__BET_OK      =  1 # bet is calculated and valid
-__BET_OPEN    = -1 # bet is still uncalculated
-__BET_INVALID = -2 # bet is invalid
-__BET_MISSING = -3 # user has no bet 
+__BET_OK = 1  # bet is calculated and valid
+__BET_OPEN = -1  # bet is still uncalculated
+__BET_INVALID = -2  # bet is invalid
+__BET_MISSING = -3  # user has no bet
 
-__DEBUG = 0
+__DEBUG = 1
 
 firebase_url = ""
 season = "2015"
 operation = 0
 
+
 def printUsage():
-	print ("scores.py -c -r -u|--user <user name> -g|--gp <gp_id> --gr <gp_results> --qr <qual results> --fl <fastest lap> --fb <firebase>")
+    print (
+    "scores.py -c -r -u|--user <user name> -g|--gp <gp_id> --gr <gp_results> --qr <qual results> --fl <fastest lap> --fb <firebase>")
 
-def main(argv):	
-	global operation, firebase_url, season
 
-        logging.basicConfig(format='%(asctime)s [%(levelname)s]:%(message)s', level=logging.INFO, datefmt='%d/%m/%Y %H:%M:%S')
+def main(argv):
+    global operation, firebase_url, season
 
-	try:
-		opts, args = getopt.getopt(argv,"hcru:", ["user=", "season=", "gp=", "gr=", "qr=", "fl=", "fb="])
-	except getopt.GetoptError:
-		printUsage()
-		sys.exit(2)
-	for opt, arg in opts:
-		if opt == '-h':
-			printUsage()
-			sys.exit()
-		if opt == '-c':
-			logging.info ("Check bet values.")			
-			operation = __CHECK_BETS
-		if opt == '-r':
-			logging.info ("Push results.")			
-			operation = __PUSH_RESULTS
-		if opt in ("-u", "--user"):			
-			user_id = arg
-		else: 
-			user_id = ''
-		if opt in ("--gp"):
-			gp_id = arg		
-		if opt in ("--gr"):
-			gresults = arg
-		if opt in ("--qr"):
-			qresults = arg
-		if opt in ("--fl"):
-			fastest = arg
-		if opt in ("--fb"):
-			firebase_url = arg
-                if opt in ("--season"):
-                        season = arg
+    logging.basicConfig(format='%(asctime)s [%(levelname)s]:%(message)s', level=logging.INFO,
+                        datefmt='%d/%m/%Y %H:%M:%S')
 
-	if not firebase_url:
-		logging.error ("No target firebase defined!!!!")
-		sys.exit()     
-	else:
-		firebase_url = "https://" + firebase_url + ".firebaseio.com/" + season
-		logging.info ("Target firebase is " + firebase_url)
+    try:
+        opts, args = getopt.getopt(argv, "hcru:", ["user=", "season=", "gp=", "gr=", "qr=", "fl=", "fb="])
+    except getopt.GetoptError:
+        printUsage()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            printUsage()
+            sys.exit()
+        if opt == '-c':
+            logging.info("Check bet values.")
+            operation = __CHECK_BETS
+        if opt == '-r':
+            logging.info("Push results.")
+            operation = __PUSH_RESULTS
+        if opt in ("-u", "--user"):
+            user_id = arg
+        else:
+            user_id = ''
+        if opt in ("--gp"):
+            gp_id = arg
+        if opt in ("--gr"):
+            gresults = arg
+        if opt in ("--qr"):
+            qresults = arg
+        if opt in ("--fl"):
+            fastest = arg
+        if opt in ("--fb"):
+            firebase_url = arg
+        if opt in ("--season"):
+            season = arg
 
-	if operation == __CHECK_BETS:
-		checkBetvalues(gp_id, user_id)
-	elif operation == __PUSH_RESULTS:
-		gpresultlist = gresults.split(',')
-		qresultlist = qresults.split(',')		
-		pushResults(gp_id, gpresultlist, qresultlist, fastest)
+    if not firebase_url:
+        logging.error("No target firebase defined!!!!")
+        sys.exit()
+    else:
+        firebase_url = "https://" + firebase_url + ".firebaseio.com/" + season
+        logging.info("Target firebase is " + firebase_url)
+
+    if operation == __CHECK_BETS:
+        checkBetvalues(gp_id, user_id)
+    elif operation == __PUSH_RESULTS:
+        gpresultlist = gresults.split(',')
+        qresultlist = qresults.split(',')
+        pushResults(gp_id, gpresultlist, qresultlist, fastest)
+
 
 def checkBetvalues(gp_id, user_id):
-	gpdata = calendar.getCalendarData(firebase_url, season, gp_id)
-	userlist = profiles.getUserData(firebase_url, user_id)
-	
-	logging.info ("Checking bet values for gp = " + gp_id)						
-	for user in userlist:
-                logging.info ("-----------------------------------------------------------------")
-		userid = user['userid']
-		currentbet = dict()
-		logging.info ("Checking bet values for user = " + user['userid'])					          
-		
-		if 'bets' in user and gp_id in user['bets']:			
-			logging.info ("User " + userid + " has bet for " + gp_id )
-			userbets = user['bets']
-			currentbet  = userbets[gp_id]
-			if 'results' in gpdata:
-				currentbet = calculateScore(currentbet, gpdata['results'])
-				
-		else: 
-			logging.info ("User " + userid + " has no bets for gp " + gp_id)
-			currentbet = dict()
-			currentbet['status'] = __BET_MISSING
-			currentbet['gp_id'] = gp_id
-			currentbet['gp_name'] = gpdata['gp_short_name']
-			currentbet['userid'] = userid
-			currentbet['totalpoints'] = 0
-			
-		query = "/users/" + str(userid) + "/bets/" + str(gp_id) + ".json" 
-		firebase.curlPut(firebase_url + query, json.dumps(currentbet))
-		query = "/users/" + str(userid) + "/scores/" + str(gp_id) + ".json" 
-		firebase.curlPut(firebase_url + query, json.dumps(currentbet))
-		query = "/scores/" + season + "/" + str(gp_id) + "/" + str(userid) + ".json" 
-		firebase.curlPut(firebase_url + query, json.dumps(currentbet))		
-		query = "/calendar/" + season + "/" + str(gp_id) + "/scores/" + str(userid) + ".json" 
-		firebase.curlPut(firebase_url + query, json.dumps(currentbet))		
-		
-	if gpdata['gp_status'] < __GP_QUAL:
-		closeGP(gpdata)
+    gpdata = calendar.getCalendarData(firebase_url, season, gp_id)
+    userlist = profiles.getUserData(firebase_url, user_id)
+
+    logging.info("Checking bet values for gp = " + gp_id)
+    for user in userlist:
+        logging.info("-----------------------------------------------------------------")
+        userid = user['userid']
+
+        logging.info("Checking bet values for user = " + user['userid'])
+
+        currentbet = dict()
+        for bet in user['bets']:
+            if bet and bet['gp_id'] == gp_id:
+                logging.debug("found bet " + json.dumps(bet))
+                currentbet = bet
+
+        if currentbet:
+            logging.info("User " + userid + " has bet for " + gp_id)
+            currentbet = calculateScore(currentbet, gpdata['results'])
+
+        else:
+            logging.info("User " + userid + " has no bets for gp " + gp_id)
+            currentbet = dict()
+            currentbet['status'] = __BET_MISSING
+            currentbet['gp_id'] = gp_id
+            currentbet['gp_name'] = gpdata['gp_short_name']
+            currentbet['userid'] = userid
+            currentbet['totalpoints'] = 0
+
+        query = "/users/" + str(userid) + "/bets/" + str(gp_id) + ".json"
+        firebase.curlPut(firebase_url + query, json.dumps(currentbet))
+        query = "/users/" + str(userid) + "/scores/" + str(gp_id) + ".json"
+        firebase.curlPut(firebase_url + query, json.dumps(currentbet))
+        query = "/scores/" + str(gp_id) + "/" + str(userid) + ".json"
+        firebase.curlPut(firebase_url + query, json.dumps(currentbet))
+        query = "/calendar/" + str(gp_id) + "/scores/" + str(userid) + ".json"
+        firebase.curlPut(firebase_url + query, json.dumps(currentbet))
+
+    if gpdata['gp_status'] < __GP_QUAL:
+        closeGP(gpdata)
+
 
 def closeGP(gpdata):
-	logging.info ("Close gp " + gpdata['gp_id'])
-	gpdata['gp_status'] = __GP_CLOSED
-	calendar.pushGpData(firebase_url, season, gpdata)
-					
+    logging.info("Close gp " + gpdata['gp_id'])
+    gpdata['gp_status'] = __GP_CLOSED
+    calendar.pushGpData(firebase_url, season, gpdata)
+
+
 def calculateScore(validbet, results):
-	totalpoints = 0
-	hiddenpoints = 0
-	isbetvalid = isBetValid(validbet)
-	if (('qlresults' in results) & ('qbets' in validbet)):
-		logging.info ("Calculate qualification points")
-		returnvalue = calculateResult(validbet['qbets'], results['qlresults'])
-		validbet['qbets'] = returnvalue['calculatedbet']
-		totalpoints += returnvalue['totalpoints']
-		validbet['qpoints'] = returnvalue['totalpoints']
+    totalpoints = 0
+    hiddenpoints = 0
+    isbetvalid = isBetValid(validbet)
+    logging.debug("bet " + json.dumps(validbet['qbets']))
+    logging.debug("results " + json.dumps(results))
+    if ('qlresults' in results) & ('qbets' in validbet):
+        logging.info("Calculate qualification points")
+        returnvalue = calculateResult(validbet['qbets'], results['qlresults'])
+        validbet['qbets'] = returnvalue['calculatedbet']
+        totalpoints += returnvalue['totalpoints']
+        validbet['qpoints'] = returnvalue['totalpoints']
 
-	if (('gpresults' in results) & ('gpbets' in validbet)):
-		logging.info ("Calculate race points")
-		returnvalue = calculateResult(validbet['gpbets'], results['gpresults'])
-		validbet['gpbets'] = returnvalue['calculatedbet']
-		totalpoints += returnvalue['totalpoints']
-		hiddenpoints = returnvalue['hiddenpoints']
-		validbet['gppoints'] = returnvalue['totalpoints']
+    if ('gpresults' in results) & ('gpbets' in validbet):
+        logging.info("Calculate race points")
+        returnvalue = calculateResult(validbet['gpbets'], results['gpresults'])
+        validbet['gpbets'] = returnvalue['calculatedbet']
+        totalpoints += returnvalue['totalpoints']
+        hiddenpoints = returnvalue['hiddenpoints']
+        validbet['gppoints'] = returnvalue['totalpoints']
 
-	if (('fastestlap' in results) & ('fastestlap' in validbet)):
-		logging.info ("Calculate fastest lap points")
-		flresult = results['fastestlap']
-		bet = validbet['fastestlap']
-		if flresult['d_id'] == bet['d_id']:
-			logging.info ("Fastest lap " + bet['d_id'] + " matches and yields " + str(flresult['points']) + " points!!")
-			bet['points'] = flresult['points']
-			totalpoints+= flresult['points']
-		else:
-			bet['points'] = 0;
-		
-		validbet['fastestlap'] = bet
+    if ('fastestlap' in results) & ('fastestlap' in validbet):
+        logging.info("Calculate fastest lap points")
+        flresult = results['fastestlap']
+        bet = validbet['fastestlap']
+        if flresult['d_id'] == bet['d_id']:
+            logging.info("Fastest lap " + bet['d_id'] + " matches and yields " + str(flresult['points']) + " points!!")
+            bet['points'] = flresult['points']
+            totalpoints += flresult['points']
+        else:
+            bet['points'] = 0;
 
-		
-	validbet['totalpoints'] = totalpoints
-	validbet['hiddenpoints'] = hiddenpoints
-	validbet['status'] = isbetvalid
-	return validbet
+        validbet['fastestlap'] = bet
+
+    validbet['totalpoints'] = totalpoints
+    validbet['hiddenpoints'] = hiddenpoints
+    validbet['status'] = isbetvalid
+    return validbet
+
 
 def isBetValid(validbet):
-	if 'qbets' not in validbet and 'gpbets' not in validbet and 'fastestlap' not in validbet:
-		logging.info ("Bet is not valid.")
-		return __BET_INVALID
-	else:
-                logging.info ("Bet is ok.")
-		return __BET_OK
-		
+    if 'qbets' not in validbet and 'gpbets' not in validbet and 'fastestlap' not in validbet:
+        logging.info("Bet is not valid.")
+        return __BET_INVALID
+    else:
+        logging.info("Bet is ok.")
+        return __BET_OK
+
 
 def calculateResult(validbet, result):
-	totalpoints = 0
-	hiddenpoints = 0
-	for key in result:
-		position = key['position']
-		bet = validbet[position - 1];
-		if bet['driverid'] == key['driverid']:
-			logging.info ("Position (" + str(position) + ") matches " + bet['driverid'] + " and gives " + str(key['points']) + " points!!")
-			bet['points'] = key['points']
-			totalpoints += key['points']
-			logging.debug ("factorial for " + str(key['points']) + " = " + str(math.factorial(key['points'])))
-			hiddenpoints += math.factorial(key['points'])
-		else: 
-			logging.debug ("No match " + bet['driverid'])
-			bet['points'] = 0
-		validbet[position - 1] = bet
+    totalpoints = 0
+    hiddenpoints = 0
+    for key in result:
+        position = key['position']
+        bet = validbet[position - 1];
+        if bet['driverid'] == key['driverid']:
+            logging.info("Position (" + str(position) + ") matches " + bet['driverid'] + " and gives " + str(
+                key['points']) + " points!!")
+            bet['points'] = key['points']
+            totalpoints += key['points']
+            logging.debug("factorial for " + str(key['points']) + " = " + str(math.factorial(key['points'])))
+            hiddenpoints += math.factorial(key['points'])
+        else:
+            logging.debug("No match " + bet['driverid'])
+            bet['points'] = 0
+        validbet[position - 1] = bet
 
-	logging.info ("Bet yields " + str(totalpoints) + " points!")
-	returnvalue = dict()
-	returnvalue['totalpoints'] = totalpoints
-	returnvalue['hiddenpoints'] = hiddenpoints
-	returnvalue['calculatedbet'] = validbet
-	return returnvalue
+    logging.info("Bet yields " + str(totalpoints) + " points!")
+    returnvalue = dict()
+    returnvalue['totalpoints'] = totalpoints
+    returnvalue['hiddenpoints'] = hiddenpoints
+    returnvalue['calculatedbet'] = validbet
+    return returnvalue
+
 
 def pushResults(gpid, gpresultlist, qresultlist, fastest):
-#        if (logging.isEnabledFor(logging.DEBUG)):
-        logging.debug ("Gp results  ", json.dumps(gpresultlist))
-        logging.debug ("Qu results  ", json.dumps(qresultlist))
-        logging.debug ("Fastest lap ", json.dumps(fastest))
-        
-	driverlist = drivers.getAllDriverData(firebase_url, season)
-	gpdata = calendar.getCalendarData(firebase_url, season, gpid)
+    #        if (logging.isEnabledFor(logging.DEBUG)):
+    logging.debug("Gp results  " + json.dumps(gpresultlist))
+    logging.debug(qresultlist)
+    logging.debug("Fastest lap " + json.dumps(fastest))
 
-	results = dict()
+    driverlist = drivers.getAllDriverData(firebase_url, season)
+    gpdata = calendar.getCalendarData(firebase_url, season, gpid)
 
-	if fastest:
-		fastestlap = dict()
-		driver = driverlist[fastest]
-		fastestlap['d_id'] = driver['d_id']
-		fastestlap['info'] = driver['d_name']
-		fastestlap['points'] = 3
-		results['fastestlap'] = fastestlap
+    logging.debug(driverlist)
 
-	results['qlresults'] = []
-	results['gpresults'] = []
+    results = dict()
 
-	for idx, val in enumerate(qresultlist):
-		if val in driverlist:
-			driver = driverlist[val]
-			result = dict()
-			result['driverid'] = driver['d_id']
-			result['position'] = idx + 1
-			result['points'] = len(qresultlist) - idx
-			result['info'] = driver['d_name']
-			results['qlresults'].append(result)
-			gpdata['gp_status'] = __GP_QUAL
-	
-	for idx, val in enumerate(gpresultlist):
-		if val in driverlist:
-			driver = driverlist[val]
-			result = dict()
-			result['driverid'] = driver['d_id']
-			result['position'] = idx + 1
-			result['points'] = len(gpresultlist) - idx
-			result['info'] = driver['d_name']
-			results['gpresults'].append(result)
-			gpdata['gp_status'] = __GP_RACE
-	
-	gpdata['results'] = results
-	query = "/calendar/" + str(gpdata['gp_year']) + "/" + str(gpdata['gp_id']) + ".json"
-	logging.info ("Pushing results to " + query)
-	firebase.curlPut(firebase_url + query, json.dumps(gpdata))
+    if fastest:
+        fastestlap = dict()
+        driver = driverlist[fastest]
+        fastestlap['d_id'] = driver['d_id']
+        fastestlap['info'] = driver['d_name']
+        fastestlap['points'] = 3
+        results['fastestlap'] = fastestlap
+
+    results['qlresults'] = []
+    results['gpresults'] = []
+
+    for idx, val in enumerate(qresultlist):
+        logging.info(val)
+        if val in driverlist:
+            driver = driverlist[val]
+            result = dict()
+            result['driverid'] = driver['d_id']
+            result['position'] = idx + 1
+            result['points'] = len(qresultlist) - idx
+            result['info'] = driver['d_name']
+            results['qlresults'].append(result)
+            gpdata['gp_status'] = __GP_QUAL
+
+    for idx, val in enumerate(gpresultlist):
+        if val in driverlist:
+            driver = driverlist[val]
+            result = dict()
+            result['driverid'] = driver['d_id']
+            result['position'] = idx + 1
+            result['points'] = len(gpresultlist) - idx
+            result['info'] = driver['d_name']
+            results['gpresults'].append(result)
+            gpdata['gp_status'] = __GP_RACE
+
+    gpdata['results'] = results
+    query = "/calendar/" + str(gpdata['gp_id']) + ".json"
+    logging.info("Pushing results to " + query)
+    firebase.curlPut(firebase_url + query, json.dumps(gpdata))
+
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
-	print "\n"
-
+    main(sys.argv[1:])
+    print "\n"
