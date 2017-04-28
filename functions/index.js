feb6bf5c6c46749737110d634b4cc95e5f915489
Niki Eskola <niki.eskola@veikkaus.fi>
2017-04-29 00:06:10 +0300

    added doubled bets counter

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 var functions = require('firebase-functions');
+var json2html = require('node-json2html');
 const admin = require('firebase-admin');
 
 admin.initializeApp(functions.config().firebase);
@@ -20,18 +21,24 @@ exports.betstatus = functions.https.onRequest((req, res) => {
     res.set("Access-Control-Allow-Methods", "GET");
     res.set("Access-Control-Allow-Headers", "Content-Type");
 
-    var bets = {};
+    var bets = { doubled: 0 };
     admin.database().ref(req.query.season).child('users').once('value').then(function (snapshot) {
         var gp = req.query.gp;
         userlist = snapshot.val();
         for (user in userlist) {
-            bets[user] = false;
+            bets[user] = { bet: false };
             for (bet in userlist[user].bets) {
                 if (bet != null && userlist[user].bets[bet].gp_id == gp) {
-                    bets[user] = true;
-                } 
+                    bets[user].bet = true;
+                    if (userlist[user].bets[bet].doubled) {
+                        bets.doubled = bets.doubled + 1;
+                    }
+                }
             }
         }
+        //var transform = {"<>":"div","html":"${bets.user} ${bets.user.bet}"};
+
+        // res.status(200).send(json2html.transform(bets,transform));
         res.status(200).send(bets);
     });
 });
