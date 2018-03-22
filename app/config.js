72b87cdf92646d2d090a5fe8d6d78494bab3f210
Niki Eskola <niki.eskola@veikkaus.fi>
2018-03-22 10:23:19 +0200

    added todo for firebase url description

--- app/config.js
+++ app/config.js
@@ -4,4 +4,5 @@ var season = "2018";
 
 var debug = false;
 
+// TODO: this should be overwritten in firebase env if found or parsed from url location
 var FIREBASE = new Firebase('https://f1kaapo.firebaseio.com/' + season);
