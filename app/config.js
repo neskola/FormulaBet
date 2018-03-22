b5244ef52dfa2f5ab84a040d625c0112af9a4cfe
Niki Eskola <niki.eskola@veikkaus.fi>
2018-03-22 10:21:37 +0200

    use production url as default

--- app/config.js
+++ app/config.js
@@ -4,4 +4,4 @@ var season = "2018";
 
 var debug = false;
 
-var FIREBASE = new Firebase('https://test-f1kaapo.firebaseio.com/' + season);
+var FIREBASE = new Firebase('https://f1kaapo.firebaseio.com/' + season);
